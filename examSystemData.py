b89195b9eee7a0fde3490fcaa71e43acb479cf67
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-05 19:06:54 +0200

    0.1.8 - defined Question class, fixed destructors + default constructors

--- examSystemData.py
+++ examSystemData.py
@@ -1,5 +1,5 @@
 class Department:
-    def __init__(self, dep_name, coordinator_name):
+    def __init__(self, dep_name='', coordinator_name=''):
         self.dep_name = dep_name
         self.coordinator_name = coordinator_name
 
@@ -8,17 +8,29 @@ class Department:
 
 
 class Course:
-    def __init__(self, course_name, lecturer_name, year_of_course):
+    def __init__(self, course_name='', lecturer_name='', year_of_course=''):
         self.course_name = course_name
         self.lecturer_name = lecturer_name
         self.year_of_course = year_of_course
-        course_dep = Department('', '')
+        course_dep = Department()
 
     def __del__(self):
         print('Course obj deleted')
 
 
+class Question:
+    def __init__(self, question_info=dict):
+        self.question_info = question_info
+
+    def __del__(self):
+        print('Question obj deleted')
+
+
 class Exam:
     def __init__(self, exam_date):
         self.exam_date = exam_date
-        exam_course = Course('', '', '')
+        exam_course = Course()
+        question_list = Question()
+
+    def __del__(self):
+        print('Exam obj deleted')
