e60c79b0c91b4a6d8814684efe90602d9dc48817
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-05 18:51:55 +0200

    0.1.6 - defined Exam Class, added composition

--- examSystemData.py
+++ examSystemData.py
@@ -12,7 +12,13 @@ class Course:
         self.course_name = course_name
         self.lecturer_name = lecturer_name
         self.year_of_course = year_of_course
+        course_dep = Department('', '')
 
     def __del__(self):
         print('Course obj deleted')
 
+
+class Exam:
+    def __init__(self, exam_date):
+        self.exam_date = exam_date
+        exam_course = Course('', '', '')
