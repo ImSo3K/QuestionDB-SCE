3b3a6cd35bf17c05af5b13b5d6dc45b89bb54dc3
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-06 17:16:47 +0200

    0.2.5 - added get_question_info func

--- textData.py
+++ textData.py
@@ -1,5 +1,21 @@
-def get_exam_info(obj):
+import examSystemData
+
+
+def get_exam_info(obj: examSystemData.Exam):
     with open('Exams.txt') as f:
         line = f.read().split()
     obj.exam_date = line[0]
     obj.exam_semester = line[1]
+
+
+def get_question_info(obj: examSystemData.Exam):
+    with open('Questions.txt') as f:
+        for line in f:
+            if not line.strip():
+                break
+            line = line.split()
+            i = 0
+            for key in obj.question.question_info.keys():
+                obj.question.question_info[key] = line[i]
+                i += 1
+            obj.question_list.append(obj.question)
