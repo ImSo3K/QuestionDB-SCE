3827720797103efedbfe256ec8398ad07de0a34e
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-06 18:45:41 +0200

    added get_course_info func

--- textData.py
+++ textData.py
@@ -14,8 +14,17 @@ def get_question_info(obj: examSystemData.Exam):
             if not line.strip():
                 break
             line = line.split()
-            i = 0
-            for key in obj.question.question_info.keys():
-                obj.question.question_info[key] = line[i]
-                i += 1
-            obj.question_list.append(obj.question)
+            empty_question = examSystemData.Question()
+            for key in empty_question.question_info.keys():
+                empty_question.question_info[key] = line[0]
+                line = line[1:]
+            obj.question_list.append(empty_question)
+
+
+def get_course_info(obj_course: examSystemData.Course, obj_exam: examSystemData.Exam):
+    with open('Courses.txt') as f:
+        line = f.read().split()
+    obj_course.course_name = line[0]
+    obj_course.lecturer_name = line[1]
+    obj_course.year_of_course = line[2]
+    obj_course.exam = obj_exam
