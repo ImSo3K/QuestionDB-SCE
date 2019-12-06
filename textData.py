7d81049586274cedbfd0b9b0961cbe4ef5ea3194
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-06 15:34:28 +0200

    0.2.3 - soft reset

--- textData.py
+++ textData.py
@@ -1,21 +1,5 @@
-import examSystemData
-
-def get_department_list(obj):
-    fo_dep = open('Departments.txt', 'r')
-    line = fo_dep.readline().rstrip()
-    obj.dep_name = line
-    line = fo_dep.readline().rstrip()
-    obj.coordinator_name = line
-    fo_dep.close()
-
-
-def get_courses_list(obj):
-    fo_course = open('Courses.txt', 'r')
-    line = fo_course.readline().rstrip()
-    obj.course_name = line
-    line = fo_course.readline().rstrip()
-    obj.lecturer_name = line
-    line = fo_course.readline().rstrip()
-    obj.year_of_course = line
-    fo_course.close()
-    
+def get_exam_info(obj):
+    with open('Exams.txt') as f:
+        line = f.read().split()
+    obj.exam_date = line[0]
+    obj.exam_semester = line[1]
