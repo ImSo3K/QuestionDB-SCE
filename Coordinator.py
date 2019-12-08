fa76827ade9079edaa4c46452f44303fc64000e6
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-08 14:20:26 +0200

    0.0.0 - user functions

---
+++ Coordinator.py
@@ -0,0 +1,13 @@
+import examSystemData
+
+
+print('You can :')
+print('1 - add a lecturer to the system\n2 - edit lecturer info')
+print('3 - add a solution\n4 - edit a question')
+print('5 - display a question\n6 - filter the questions shown by criteria')
+print('Press 6 to exit')
+choice = 0
+while 0 <= choice <= 6:
+    choice = int(input())
+    if choice == 1:
+        input('')
