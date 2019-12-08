405ab5cd0d855521ad34557c267e87a7d37154cf
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-08 15:23:50 +0200

    0.5.0 - added QuestionData and SolutionsData with the proper functionalities

---
+++ QuestionAndSolutions.py
@@ -0,0 +1,40 @@
+import examSystemData
+
+
+class QuestionData:
+    def __init__(self, obj: examSystemData.Exam):
+        self.question_database = obj.question_list
+
+    def add_question(self, list_of_values: list):
+        empty_question = examSystemData.Question()
+        for key in empty_question.question_info.keys():
+            empty_question.question_info[key] = list_of_values[0]
+            list_of_values = list_of_values[1:]
+        self.question_database.append(empty_question)
+
+    def edit_question(self, v_key='', v_value=''):
+        for i in enumerate(self.question_database):
+            obj = examSystemData.Question()
+            obj.question_info = self.question_database[i]
+            for key in obj.question_info.keys():
+                if key == v_key:
+                    obj.question_info[key] = v_value
+
+    def print_sorted_question_list_by_diff(self):
+        print(sorted(self.question_database, key=lambda i: i['Difficulity']))
+
+    def print_filtered_question_list_by_diff(self, v_value=''):
+        for i in enumerate(self.question_database):
+            obj = examSystemData.Question()
+            obj.question_info = self.question_database[i]
+            for value in obj.question_info.values():
+                if value == v_value:
+                    examSystemData.Question.print_question_info(self.question_database[i])
+
+
+class SolutionData:
+    def __init__(self):
+        self.solution_list = []
+
+    def add_sol(self, sol=''):
+        self.solution_list.append(sol)
