aa33206fc05c69745396d3d59383755b2cc802ba
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-08 18:18:25 +0200

    1.5.5 - added menu for the needed Users

--- main_menu.py
+++ main_menu.py
@@ -2,6 +2,9 @@ import examSystemData
 import textData
 import pdf2png
 import png2docx
+import Coordinator
+import Lecturer
+import Student
 
 
 def init_and_menu():
@@ -16,6 +19,7 @@ def init_and_menu():
     print('Press 2 to print Course data')
     print('Press 3 to print Exam data')
     print('Press 4 to convert pdf2image & then image2docx')
+    print('Press 5 to log in')
     print('Press 0 to exit')
     choice = 1
     while 1 <= choice <= 4:
@@ -37,6 +41,22 @@ def init_and_menu():
         elif choice == 4:
             pdf2png.pdf_2_image()
             png2docx.image_2_docx()
+
+        elif choice == 5:
+            log_choice = 1
+            while 1 <= log_choice <= 3:
+                log_choice = int(input())
+                print('1 - to log in as Coordinator')
+                print('2 - to log in as Lecturer')
+                print('3 - to log in as Student')
+                if log_choice == 1:
+                    Coordinator.Coordinator_menu(obj1)
+
+                if log_choice == 2:
+                    Lecturer.Lecturer_menu(obj1)
+
+                if log_choice == 3:
+                    Student.Student_menu(obj1)
 
         elif choice == 0:
             break
