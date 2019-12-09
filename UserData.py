93d8d44013418ac4690ca430c3a5b7aff25e0132
Sharon Yaroshetsky <imso3k@gmail.com>
2019-12-09 13:33:57 +0200

    0.5.2 - fixed logic in edit_lecturer_info func

--- UserData.py
+++ UserData.py
@@ -8,4 +8,6 @@ class UserData:
     def edit_lecturer_info(self, lecturer_first_name='', lecturer_edit=''):
         for i, name in enumerate(self.lecturer_list):
             if lecturer_first_name == name[0]:
-                self.lecturer_list.insert(i, lecturer_edit)
+                full_name = self.lecturer_list[i]
+                new_name = (lecturer_edit, full_name[1])
+                self.lecturer_list[i] = new_name
