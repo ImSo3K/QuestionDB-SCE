a8e4da93ef362d599f6c9ff55b29d0dacf6ed7dd
Sharon Yaroshetsky <imso3k@gmail.com>
2020-01-10 21:48:37 +0200

    2.0.0 - refactored the whole file, fixed wrong input crashes & logic

--- main_menu.py
+++ main_menu.py
@@ -2,9 +2,7 @@ import examSystemData
 import textData
 import pdf2png
 import png2docx
-import Coordinator
-import Lecturer
-import Student
+import Actions
 
 
 def init_and_menu():
@@ -21,49 +19,57 @@ def init_and_menu():
     print('Press 4 to convert pdf2image & then image2docx')
     print('Press 5 to log in')
     print('Press 0 to exit')
-    choice = 1
-    while 1 <= choice <= 4:
-        choice = int(input())
+    while True:
+        choice = input()
 
-        if choice == 1:
+        if choice == '1':
             print(f'Department Name : {obj3.dep_name}\n' + f'Department Coordinator Name : {obj3.coordinator_name}')
 
-        elif choice == 2:
+        elif choice == '2':
             print(f'Course Name : {obj2.course_name}\n' + f'Course Lecturer Name : {obj2.lecturer_name}')
             print(f'Year Of Course : {obj2.year_of_course}')
 
-        elif choice == 3:
+        elif choice == '3':
             print(f'Exam Date : {obj2.exam.exam_date}\n' + f'Exam semester : {obj2.exam.exam_semester}')
             print('Question list :\n')
             for i in obj2.exam.question_list:
                 examSystemData.Question.print_question_info(i)
 
-        elif choice == 4:
+        elif choice == '4':
             pdf2png.pdf_2_image()
             png2docx.image_2_docx()
 
-        elif choice == 5:
-            log_choice = 1
-            while 1 <= log_choice <= 3:
+        elif choice == '5':
+            while True:
                 print('1 - to log in as Coordinator')
                 print('2 - to log in as Lecturer')
                 print('3 - to log in as Student')
-                log_choice = int(input())
-                if log_choice == 1:
-                    Coordinator.Coordinator_menu(obj1)
+                print('0 - to exit log-in menu')
+                log_choice = input()
+                if log_choice == '1':
+                    Actions.interface('Coordinator', obj1)
 
-                if log_choice == 2:
-                    Lecturer.Lecturer_menu(obj1)
+                elif log_choice == '2':
+                    Actions.interface('Lecturer', obj1)
 
-                if log_choice == 3:
-                    Student.Student_menu(obj1)
+                elif log_choice == '3':
+                    Actions.interface('Student', obj1)
 
-        elif choice == 0:
+                elif log_choice == '0':
+                    break
+
+                else:
+                    print('Invalid Choice, Try Again')
+
+        elif choice == '0':
             break
 
         else:
-            print('Invalid Choice')
+            print('Invalid Choice, Try Again')
 
 
 print('initializing objects with the data from the given text files')
 init_and_menu()
+# re-arranged the user's files, way less copy paste code now, way more orginized.
+# convertion worked only on my spesific dir, changed it to work globally with os library.
+# deletes png files after its done.
