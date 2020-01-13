680090247169539f9729214ce2b8c2e3388e1a45
Sharon Yaroshetsky <imso3k@gmail.com>
2020-01-13 12:01:52 +0200

    0.1.3 - edited output format

--- logger.py
+++ logger.py
@@ -4,10 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 f_handler = logging.FileHandler('file.log')
-#logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-f_format = logging.Formatter('%(asctime)s - %(funcName)5s() - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(filename)s - %(funcName)5s() - %(levelname)s - %(message)s', datefmt="%d-%b-%y %H:%M:%S")
 f_handler.setFormatter(f_format)
 logger.addHandler(f_handler)
 logger.setLevel(logging.DEBUG)
-#logger.info('Admin logged out')
-
